cf43d6659e90115de33ac1e9b079ed6365df73df
Mohamed Ali <52932544+muhamed3li@users.noreply.github.com>
2022-02-23 00:40:28 +0200

    add crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
